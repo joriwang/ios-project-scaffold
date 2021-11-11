fd0846b3115cadb8329ea7ec7aa452d0c36f6670
Jori <wanghui@qing-jin.com>
2021-11-12 02:53:13 +0800

    调整加密类结构

--- SwiftApp.xctemplate/Project/Tools/Security.swift
+++ SwiftApp.xctemplate/Project/Tools/Security.swift
@@ -9,7 +9,24 @@ import Foundation
 import CryptoSwift
 
 final class Security {
-    static func cy_AES(_ plainText: String) -> String {
+    static func aesDecrypt(_ cipherText: String, key: [UInt8], chip: BlockMode, padding: Padding = .pkcs7) -> String? {
+        do {
+            let cipherData = cipherText.data(using: .utf8)!
+            let aes = try AES(key: key, blockMode: chip, padding: padding)
+            let plainBytes = try aes.decrypt(cipherData.bytes)
+            let plainData = Data(plainBytes)
+            return String(data: plainData, encoding: .utf8)
+        } catch {
+            log.e("[Security][AES][Decrypt][Failed][Chip]\(chip.self)[Padding]\(padding)[Error]\(error.localizedDescription)[CipherText]\(cipherText)")
+            return nil
+        }
+        
+    }
+}
+
+// MARK: - CY
+extension Security {
+    static func cy_AESEncry(_ plainText: String) -> String {
         let debugBeginDate = Date()
         let plainData = plainText.data(using: .utf8)!
         assert(plainData.count <= 12288, "aes 明文长度太长: \(plainData.count)")
@@ -27,19 +44,5 @@ final class Security {
         let debugEndDate = Date()
         log.d("[Security][AES] 耗时: \(debugEndDate.timeIntervalSince(debugBeginDate))秒")
         return base64
-    }
-    
-    static func aesDecrypt(_ cipherText: String, key: [UInt8], chip: BlockMode, padding: Padding = .pkcs7) -> String? {
-        do {
-            let cipherData = cipherText.data(using: .utf8)!
-            let aes = try AES(key: key, blockMode: chip, padding: padding)
-            let plainBytes = try aes.decrypt(cipherData.bytes)
-            let plainData = Data(plainBytes)
-            return String(data: plainData, encoding: .utf8)
-        } catch {
-            log.e("[Security][AES][Decrypt][Failed][Chip]\(chip.self)[Padding]\(padding)[Error]\(error.localizedDescription)[CipherText]\(cipherText)")
-            return nil
-        }
-        
     }
 }
